a158f3d7ee430dc35a0eaed7e2550c271e71a42e
adw1n <adw1n@users.noreply.github.com>
2020-07-19 23:06:02 +0200

    Make discovery document cache configurable

--- discovery/oidc/oidc.go
+++ discovery/oidc/oidc.go
@@ -16,9 +16,22 @@
 
 package oidc
 
-import "github.com/okta/okta-jwt-verifier-golang/discovery"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
 
-type Oidc struct{
+	"github.com/patrickmn/go-cache"
+
+	"github.com/okta/okta-jwt-verifier-golang/discovery"
+)
+
+var metaDataCache *cache.Cache = cache.New(5*time.Minute, 10*time.Minute)
+var metaDataMu = &sync.Mutex{}
+
+type Oidc struct {
 	wellKnownUrl string
 }
 
@@ -27,6 +40,30 @@ func (d Oidc) New() discovery.Discovery {
 	return d
 }
 
-func (d Oidc) GetWellKnownUrl() string {
-	return d.wellKnownUrl
+func (d Oidc) GetJWKSUri(issuer string) (string, error) {
+	// https://developer.okta.com/docs/reference/api/oidc/#keys
+	// "Okta strongly recommends retrieving keys dynamically with the JWKS published in the discovery document"
+	metaDataUrl := issuer + d.wellKnownUrl
+
+	metaDataMu.Lock()
+	defer metaDataMu.Unlock()
+
+	if x, found := metaDataCache.Get(metaDataUrl); found {
+		return x.(map[string]interface{})["jwks_uri"].(string), nil
+	}
+
+	resp, err := http.Get(metaDataUrl)
+
+	if err != nil {
+		return "", fmt.Errorf("request for metadata was not successful: %s", err.Error())
+	}
+
+	defer resp.Body.Close()
+
+	md := make(map[string]interface{})
+	json.NewDecoder(resp.Body).Decode(&md)
+
+	metaDataCache.SetDefault(metaDataUrl, md)
+
+	return md["jwks_uri"].(string), nil
 }
